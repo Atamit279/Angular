fc9fea8d353c8a1e10afb5e92eaae18bc07b59b4
Dimas <daimanroyal@gmail.com>
2022-04-05 14:24:34 +0300

    Final Spring + Angular

---
+++ src/@core/dtos/MainPageDTO.ts
@@ -0,0 +1,8 @@
+export class MainPageDTO{
+  id: number = 0;
+  login: string = "";
+  password: string = "";
+  conpassword: string = "";
+  email:  string = "";
+  age: number = 0;
+}
---
+++ src/@core/main-page.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient, HttpResponse} from "@angular/common/http";
+import {MainPageDTO} from "./dtos/MainPageDTO";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MainPageService {
+  private port = "8443"
+  private url = `http://localhost:${this.port}/api/service/account`;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  save(mainPage: MainPageDTO): Observable<MainPageDTO> {
+    return this.http.post<MainPageDTO>(`${this.url}/`, mainPage);
+  }
+
+  getById(id: number): Observable<MainPageDTO> {
+    return this.http.get<MainPageDTO>(`${this.url}/${id}`)
+  }
+}
