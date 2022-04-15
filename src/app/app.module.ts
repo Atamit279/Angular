db7a3e09083e4773adf96296ba2a7b5e600ece58
Dimas <daimanroyal@gmail.com>
2022-04-15 13:48:26 +0300

    Test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,75 @@
-import { NgModule } from '@angular/core';
+import {Injector, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {DialogElementsExampleDialog, MainPageComponent} from './main-component/main-component.component';
+import {createCustomTheme} from "@angular/material/schematics/ng-add/theming/create-custom-theme";
+import {createCustomElement} from "@angular/elements";
+import {MainPageService} from "../@core/main-page.service";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {MatSliderModule} from "@angular/material/slider";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatButtonModule} from "@angular/material/button";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatCardModule} from "@angular/material/card";
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatIconModule} from "@angular/material/icon";
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import { MainMenuComponent } from './main-component/main-menu/main-menu/main-menu.component';
+import {RouterModule, Routes} from "@angular/router";
+
+const appRoutes: Routes =[
+  { path: 'menu', component: MainMenuComponent},
+  { path: 'registration', component: MainPageComponent },
+
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    MainMenuComponent
   ],
   imports: [
-    BrowserModule
+    MatTabsModule,
+    MatCheckboxModule,
+    BrowserModule,
+    MatInputModule,
+    MatButtonToggleModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSlideToggleModule,
+    MatCardModule,
+    MatDialogModule,
+    MatIconModule,
+    MatDatepickerModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(private injector: Injector) {
+    const mainPage = createCustomElement(MainPageComponent, {injector: this.injector});
+    customElements.define("main-page", mainPage)
+    const mainMenu = createCustomElement(MainMenuComponent, {injector: this.injector});
+    customElements.define("main-menu", mainMenu)
+
+  }
+
+
+
+
+}
---
+++ src/app/main-component/main-component.component.ts
@@ -0,0 +1,108 @@
+import { Component, OnInit } from '@angular/core';
+import {MainPageDTO} from "../../@core/dtos/MainPageDTO";
+import {AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators} from "@angular/forms";
+import {MainPageService} from "../../@core/main-page.service";
+import {MAT_DIALOG_DATA, MatDialog} from "@angular/material/dialog";
+import {invalid} from "@angular/compiler/src/render3/view/util";
+import {map, Observable} from "rxjs";
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-component.component.html',
+  styleUrls: ['./main-component.component.scss']
+})
+export class MainPageComponent implements OnInit {
+  form!: FormGroup;
+  editing: boolean = false;
+  mainPage: MainPageDTO = new MainPageDTO();
+  hide = true;
+
+  constructor(
+    public dialog: MatDialog,
+    private formBuilder: FormBuilder,
+    private mainPageService: MainPageService,
+
+  ) {
+  }
+
+  openDialog() {
+    this.dialog.open(DialogElementsExampleDialog);
+  }
+
+
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(data?: MainPageDTO): void {
+    this.form = this.formBuilder.group({
+      cb: [false, Validators.requiredTrue],
+      login: [[data && data.login ? data.login : ''], [Validators.required, Validators.minLength(5)]],
+      password: [[data && data.password ? data.password : ''], [Validators.required, Validators.minLength(5)]],
+      conpassword: [[data && data.conpassword ? data.conpassword : ''], [Validators.required, Validators.minLength(5)]],
+      email: [[data && data.email ? data.email : ""], [Validators.required, Validators.email, Validators.maxLength(40)]]
+       },{validator: this.checkIfMatchingPasswords('password', 'conpassword')})
+  }
+
+
+
+
+
+  checkIfMatchingPasswords(passwordKey: string, passwordConfirmationKey: string) {
+    return (group: FormGroup) => {
+      let passwordInput = group.controls[passwordKey],
+        passwordConfirmationInput = group.controls[passwordConfirmationKey];
+      if (passwordInput.value !== passwordConfirmationInput.value) {
+        return passwordConfirmationInput.setErrors({notEquivalent: true})
+      }
+      else {
+        return passwordConfirmationInput.setErrors(null);
+      }
+    }
+  }
+
+
+
+
+
+  mockMethod(): void {
+    this.form?.get('login')
+  }
+
+  toggleEditing(): boolean {
+    this.editing = !this.editing
+    return this.editing;
+  }
+
+  save(): void{
+    this.editing = false;
+    this.mainPage.login = this.form.get('login')?.value;
+    this.mainPage.password = this.form.get('password')?.value;
+    this.mainPage.conpassword = this.form.get('conpassword')?.value;
+    this.mainPage.email = this.form.get('email')?.value;
+    this.mainPageService.save(this.mainPage).subscribe(data => {
+      console.log(data);
+    }, error => {
+      alert("This login already exists");
+    });
+
+
+  }
+
+  cancel(): void{
+    this.editing = false;
+    this.form.get('login')?.setValue(this.mainPage.login);
+    this.form.get('password')?.setValue(this.mainPage.password);
+    this.form.get('conpassword')?.setValue(this.mainPage.conpassword);
+    this.form.get('email')?.setValue(this.mainPage.email);
+  }
+
+}
+@Component({
+  selector: 'dialog-elements-example-dialog',
+  templateUrl: 'dialog-elements-example-dialog.html',
+})
+export class DialogElementsExampleDialog {}
+
